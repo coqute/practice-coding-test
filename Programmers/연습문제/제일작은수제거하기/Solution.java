debfc22b128d84782c304cd9f55783c8b67fe241
COQUTE <kimjiho246@naver.com>
2025-06-21 23:51:38 +0900

    Lv.1 연습문제

---
+++ Programmers/연습문제/제일작은수제거하기/Solution.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        
+        if(arr.length == 1) {
+            return new int[]{-1};
+        }
+        
+        int min = arr[0];
+        for(int i = 1; i < arr.length; i++) {
+            if(min > arr[i]) {
+                min = arr[i];
+            }
+        }
+        
+        final int MIN = min;
+        return Arrays.stream(arr).filter(x -> x != MIN).toArray();
+    }
+}
