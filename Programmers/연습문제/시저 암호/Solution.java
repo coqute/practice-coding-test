7fe4f79752cf5bd1ac4ed00447ad120a85004344
COQUTE <kimjiho246@naver.com>
2025-08-01 23:45:36 +0900

    Lv.1 연습문제

---
+++ Programmers/연습문제/시저 암호/Solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public String solution(String s, int n) {
+        // A ~ Z: 65 ~ 90
+        // a ~ z: 97 ~ 122
+        
+        // 1. (charAt() - 65 + n) % 25 + charAt()
+    }
+}
