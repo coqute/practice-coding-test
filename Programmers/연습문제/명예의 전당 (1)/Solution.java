c026426285dcc4ec4c460528226b2736ee38f119
COQUTE <kimjiho246@naver.com>
2025-08-13 00:49:00 +0900

    Lv.1 연습문제

--- Programmers/연습문제/명예의 전당 (1)/Solution.java
+++ Programmers/연습문제/명예의 전당 (1)/Solution.java
@@ -1,10 +1,22 @@
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Queue;
+import java.util.PriorityQueue;
 
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
+        
+        Queue<Integer> queue = new PriorityQueue<>();
+        
+        for(int i = 0; i < score.length; i++) {
+            queue.add(score[i]);
+            
+            if(queue.size() > k)
+                queue.poll();
+            
+            answer[i] = queue.peek();
+        }
+        
+        /* 기존 풀이
         List<Integer> list = new ArrayList<>();
         
         for(int i = 0; i < score.length; i++){
@@ -14,6 +26,8 @@ class Solution {
             
             answer[i] = (i < k ? list.get(0) : list.get(i - k + 1));
         }
+        */
+        
         return answer;
     }
 }
