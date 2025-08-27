e8eeaf6b96024d619994128efda998c86b959dd0
COQUTE <kimjiho246@naver.com>
2025-08-27 23:59:54 +0900

    Lv.1 Summer/Winter Coding(~2018)

---
+++ Programmers/Summer Winter Coding(~2018)/소수 만들기/Solution.java
@@ -0,0 +1,42 @@
+public class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        
+        // 첫 번째 숫자의 위치
+        for (int i = 0; i < nums.length - 2; i++) {
+            // 두 번째 숫자의 위치
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                // 세 번째 숫자의 위치
+                for (int k = j + 1; k < nums.length; k++) {
+                    // 3개의 숫자 합 구하기
+                    int sum = nums[i] + nums[j] + nums[k];
+                    
+                    // 소수인지 확인하기
+                    if (isPrime(sum)) {
+                        answer++;  // 만약 소수가 맞다면 answer는 1 증가한다.
+                    }
+                }
+            }
+        }
+        
+        return answer;  // answer 반환하기
+    }
+    
+    // 소수인지 확인하는 메서드 생성
+    private boolean isPrime(int num) {
+        
+        // num이 2인지 확인
+        if(num == 2) {
+            return true;  // 2가 맞다면 true 반환
+        }
+        
+        // 2부터 자기 자신(num) 전까지 나누어 떨어지는 숫자가 있는지 확인
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;  // 나누어 떨어진다면 false 반환
+            }
+        }
+        
+        return true;  // 나누어 떨어지지 않았다면 true 반환
+    }
+}
